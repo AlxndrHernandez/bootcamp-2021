1387fdb9b3ec087181980a012ff9f1e811916922
Alxndr <alxndr.hernandez@gmail.com>
2021-12-23 18:51:43 -0300

    modificacion letpal

--- letrasdeunapalabra/letpal.go
+++ letrasdeunapalabra/letpal.go
@@ -8,7 +8,7 @@ func main() {
 	pointer := &palabra
 	fmt.Printf("la direccion de memoria es %p, %p y su contenido es %s \n", pointer, &palabra, *pointer)
 	fmt.Println(len(palabra))
-
+	fmt.Printf("%s \n", palabra)
 	for _, p := range palabra {
 
 		fmt.Printf("%c \n", p)
