264a51560f373492bc549ba497efe16bc21d7a6d
Alxndr <alxndr.hernandez@gmail.com>
2021-12-23 00:37:41 -0300

    se agrega decvar

---
+++ decvar/decvar.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre string = "Alexander"
+	var apellido string = "Hernandez"
+	var edad int = 30
+	apellidoDos := "Munios"
+	var licencia_de_conducir = true
+	var estaturadelapersona int = 176
+	cantidadDeHijos := 2
+
+	fmt.Println(nombre, apellido, edad, apellidoDos, licencia_de_conducir, estaturadelapersona, cantidadDeHijos)
+
+}
