9e34bc0f403fed8d4b2541d5b3e7d55b1107637e
Alxndr <alxndr.hernandez@gmail.com>
2021-12-23 00:26:30 -0300

    se agrego clima

---
+++ clima/clima.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var temperatura float32 = 32.5
+	var presion float32 = 5
+	var humedad int = 150
+
+	fmt.Println("La temperatura es de:", temperatura, "con una presion atmosferica de:", presion, "y una humedad de:", humedad)
+
+}
