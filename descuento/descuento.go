13816c450730d7e86b82463df8dfb67e8caf5baa
Alxndr <alxndr.hernandez@gmail.com>
2021-12-23 12:12:07 -0300

    se agregra descuento

---
+++ descuento/descuento.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	precio := 15000
+	descuento := 30
+	total := (precio * (100 - descuento)) / 100
+
+	fmt.Println("el precio total del producto aplicando el descuento, es de:", total)
+
+}
