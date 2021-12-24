271adec153c8d3d119d0f8ccddb9a9a75df44af0
Alxndr <alxndr.hernandez@gmail.com>
2021-12-24 00:59:19 -0300

    se agrego carpeta y archivo prestamo

---
+++ prestamo/prestamo.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var edad int = 25
+	var empleado bool = true
+	var antiguedadLaboral = 0
+	var sueldo int = 200000
+
+	switch {
+	case edad < 22:
+		fmt.Println("tu edad es menor a 22 años, no puedes acceder a un credito ")
+		fallthrough
+
+	case !empleado:
+		fmt.Println("Estas desempleado, por ende no puedes acceder a un credito")
+		fallthrough
+
+	case antiguedadLaboral < 1:
+		fmt.Println("tu antiguedad laboral es menor a un año no puedes acceder a un credito")
+		fallthrough
+
+	case sueldo < 100000:
+		fmt.Println("al tener un suedo menor a 100.000 se te cobrará interez")
+
+	default:
+		fmt.Println("cumples con los requisitos")
+
+	}
+
+}
