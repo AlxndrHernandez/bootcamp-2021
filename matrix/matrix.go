a25c7578f1e2ca1674118f6a523eb3079a624d1a
Alxndr <alxndr.hernandez@gmail.com>
2021-12-29 13:49:55 -0300

    se agrega matrix

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,28 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Matrix struct {
-	valores []float64
-	alto    float64
-	ancho   float64
+	valores    []float64
+	alto       int
+	ancho      int
+	cuadratica bool
+	maxvalue   float64
 }
 
-func (m Matrix) set() {
+func (m *Matrix) set(numeros ...float64) {
 
-	fmt.Printf("\n Valores: %v\n Alto: %v\n Ancho: %v\n", m.valores, m.alto, m.ancho)
+	m.valores = numeros
 
 }
 
-func main() {
+func (m Matrix) print() {
 
-	matrix := Matrix{
-
-		valores: []float64{2, 3, 4, 5},
-		alto:    4,
-		ancho:   4,
+	if len(m.valores) == 0 {
+		fmt.Println("La matriz está vacía")
+	}
+	for fila := 0; fila <= m.alto; fila++ {
+		fmt.Println(m.valores[fila*m.ancho : fila*m.ancho+m.ancho])
 	}
 
-	matrix.set()
+	if m.alto == m.ancho {
+
+		fmt.Println("La matriz es cuadratica")
+		m.cuadratica = true
+	} else {
+
+		fmt.Println("La matriz no es cuadratica")
+		m.cuadratica = false
+	}
+	numeroMayor := m.valores[0]
+	for _, v := range m.valores {
+		if v > numeroMayor {
+			numeroMayor = v
+		}
+	}
+	fmt.Printf("EL numero mayor es: %v", numeroMayor)
+
+}
+func main() {
+
+	m := Matrix{
+
+		alto:       3,
+		ancho:      3,
+		cuadratica: true,
+		maxvalue:   0,
+	}
+	fmt.Println(m.valores)
+	m.set(1, 2, 3, 4, 5, 6, 7, 8, 9, 99, 11, 12)
+	fmt.Println(m.valores)
+	m.print()
 
 }
